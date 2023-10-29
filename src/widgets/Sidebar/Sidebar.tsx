3c8a0dd188c624df178ab5838e5884da1dd78019
Roman <roman.pavlov.1996@bk.ru>
2023-10-29 18:38:56 +0300

    EmployeeList -> sidebar + onClickItem navigate

--- src/widgets/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/Sidebar.tsx
@@ -1,6 +1,11 @@
 import { classNames } from 'shared/lib/classNames/classNames'
 import cls from './Sidebar.module.scss'
 import { SearchEmployees } from 'features/SearchEmployees'
+import { Employee, EmployeeList, getEmployees } from 'entities/Employee'
+import { useSelector } from 'react-redux'
+import { useAppDispatch } from 'shared/lib/hooks/useAppDispatch/useAppDispatch'
+import { employeeActions } from 'entities/Employee/model/slice/employeeSlice'
+import { useNavigate } from 'react-router-dom'
 
 interface SidebarProps {
     className?: string
@@ -8,8 +13,19 @@ interface SidebarProps {
 
 export const Sidebar = (props: SidebarProps) => {
     const { className } = props
+    const employees = useSelector(getEmployees)
+    const dispatch = useAppDispatch()
+    const navigate = useNavigate()
+
+    const onClickItem = (employee: Employee) => {
+        dispatch(employeeActions.setSelectedEmployee(employee))
+        navigate('/profile')
+    }
 
     return <div className={classNames(cls.Sidebar, {}, [className])}>
         <SearchEmployees />
+        {employees && <EmployeeList employees={employees}
+            onClick={onClickItem}
+            label='Результаты' />}
     </div>
 }
