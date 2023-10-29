090cb24d87879d880cf651dfaf342c584e288afe
Roman <roman.pavlov.1996@bk.ru>
2023-10-29 18:39:05 +0300

    Minor changes

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,31 +1,17 @@
+import { useNavigate } from 'react-router-dom';
 import './styles/index.scss';
 import { AppRouter } from 'app/providers/router';
-import { NavLink } from 'react-router-dom';
+import { useEffect } from 'react';
 
 const App = () => {
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        navigate('/')
+    }, [])
+
     return (
         <>
-            {/* <NavLink
-                to="/"
-                className={({ isActive }) =>
-                    `rounded-md px-3 py-2 text-sm font-medium ${
-                        isActive ? 'text-orange-400 bg-gray-900' : 'text-white'
-                    }`
-                }
-            >
-                MainPage
-            </NavLink>
-            <NavLink
-                to="/profile"
-                className={({ isActive }) =>
-                    `rounded-md px-3 py-2 text-sm font-medium ${
-                        isActive ? 'text-orange-400 bg-gray-900' : 'text-white'
-                    }`
-                }
-            >
-                ProfilePage
-            </NavLink> */}
-
             <AppRouter />
         </>
     );
