8de33f2f0945dbe919c2eae50627df768e61b6a8
Roman <roman.pavlov.1996@bk.ru>
2023-10-30 09:13:44 +0300

    Virtuoso was added

--- src/entities/Employee/ui/EmployeeList/EmployeeList.tsx
+++ src/entities/Employee/ui/EmployeeList/EmployeeList.tsx
@@ -3,6 +3,7 @@ import { EmployeeItem } from "../EmployeeItem/EmployeeItem"
 import cls from './EmployeeList.module.scss'
 import { useSelector } from "react-redux"
 import { getSelectedEmployee } from "entities/Employee/model/selectors/getSelectedEmployee"
+import { Virtuoso } from 'react-virtuoso'
 
 interface EmployeeList {
     employees?: Employee[]
@@ -14,15 +15,27 @@ export const EmployeeList = (props: EmployeeList) => {
     const { employees, onClick, label } = props
     const selectedEmployee = useSelector(getSelectedEmployee)
 
+
+    const renderEmployeeItem = (index: number, employee: Employee) => (
+        <EmployeeItem idSelectedEmployee={selectedEmployee?.id}
+            onClick={onClick}
+            item={employee}
+            key={employee.id}
+            className={cls.item} />
+    )
+
     return <>
         <h4 className={cls.title}>{label}</h4>
         <ul className={cls.EmployeeList}>
-            {employees ? employees.length > 0 ? employees.map(employee => <EmployeeItem
-                idSelectedEmployee={selectedEmployee?.id}
-                onClick={onClick}
-                item={employee}
-                key={employee.id}
-                className={cls.item} />) : <span className={cls.text}>ничего не найдено</span> : <span className={cls.text}>начните поиск</span>}
+            {employees ? employees.length > 0 ? <Virtuoso
+                style={{ height: '400px', top: '5px' }}
+                data={employees}
+
+                className={cls.list}
+                itemContent={renderEmployeeItem}
+            /> : <span className={cls.text}>ничего не найдено</span> : <span className={cls.text}>начните поиск</span>}
+
+
         </ul>
     </>
 }
